87fe553644f93834f341a8903f4189d8cbbce1e4
Seyhun Koçak <seyhunkocak11@gmail.com>
2022-05-31 01:15:04 +0300

    First Commit
    Objects are added to first and second view controllers.

---
+++ homework_2_seyhunKocak/homework_2_seyhunKocak/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  homework_2_seyhunKocak
+//
+//  Created by Seyhun Koçak on 31.05.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var amountField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+ 
+    @IBAction func confirmButtonClicked(_ sender: Any) {
+    }
+    
+}
---
+++ homework_2_seyhunKocak/homework_2_seyhunKocak/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  homework_2_seyhunKocak
+//
+//  Created by Seyhun Koçak on 31.05.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var balanceLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func depositButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func withdrawButtonClicked(_ sender: Any) {
+    }
+}
+
